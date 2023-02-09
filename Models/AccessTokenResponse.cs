a4c66e99ed7d9fba1d3a21c5c1c3af15aa932a17
Vitalii Mamontov <vintixfish@gmail.com>
2023-02-09 22:58:15 +0200

    Init

---
+++ Models/AccessTokenResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace GitHub.Statistics.Authentication.Models
+{
+    public class AccessTokenResponse
+    {
+        [JsonProperty("access_token")]
+        public string AccessToken { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthentication()
+    .AddOAuth("GitHub", "GitHub AccessToken only", o =>
+    {
+        o.ClientId = "c4b863f609b87f469301";
+        o.ClientSecret = "01d13e48ccb87bba4ef5f62f4aef401cf700ecbe";
+        o.CallbackPath = new PathString("/signin-github-token");
+        o.AuthorizationEndpoint = "https://github.com/login/oauth/authorize";
+        o.TokenEndpoint = "https://github.com/login/oauth/access_token";
+        o.SaveTokens = true;
+        o.Backchannel = new HttpClient();
+        o.Backchannel.DefaultRequestHeaders.Add("Accept", "application/json");
+    });
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
